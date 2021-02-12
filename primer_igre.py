12aa9f2a0251b3eb2e14dd621f853886f3462ebc
majbc1999 <majbc1999@gmail.com>
2021-02-13 00:00:35 +0100

    Primer igre da vidim ali deluje

---
+++ primer_igre.py
@@ -0,0 +1,29 @@
+import random
+from main import Figura, Stadion, Igralec, Game
+
+kmet11 = Figura(0,0, 'kmet')
+kmet12 = Figura(2,1, 'kmet')
+kmet21 = Figura(0,3, 'kmet')
+kmet22 = Figura(3,2, 'kralj')
+
+igralec1 = Igralec('Marjan', [kmet11, kmet12])
+igralec2 = Igralec('Tomaž', [kmet21, kmet22])
+
+igrisce = Stadion(4,4)
+
+igraa = Game([igralec1, igralec2], igrisce)
+
+def tip(igra):
+    for igralec in igra.igralca:
+        for figura in igralec.figure:
+            print(igralec.ime) 
+            print(figura.tip)
+
+def mesta(igra):
+    for igralec in igra.igralca:
+        for figura in igralec.figure:
+            print(igralec.ime, ': ', figura.tip, ' ~ ', figura.x, ',', figura.y)
+
+
+igraa.premik_figure(igralec1, 1, -1, 1)
+
