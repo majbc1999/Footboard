2f451a4dedab84608a289dc061e8a18cb71e70a8
majbc1999 <majbc1999@gmail.com>
2021-02-13 00:00:21 +0100

    Dodani vsi razredi

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+########################################### ~ POMOŽNE FUNKCIJE ~ ###########################################
 kocka = [1,2,3,4,5,6]
 
 
@@ -9,6 +10,7 @@ def met_kock():
     return [met1, met2]
 
 
+########################################### ~ FIGURA ~ ###########################################
 class Figura:
     def __init__(self, x, y, tip):
         self.x = x
@@ -22,30 +24,54 @@ class Figura:
     def ponastavi(self, a, b):
         self.x = a
         self.y = b
-    
-class Polje:
-    def __init__(self, sez):
-         self.x = sez[0]
-         self.y = sez[1]
-    
-#    def tip(self):
-#        if self.x == 0  and (self.y in 2:5):
-#            return "Gol"
-#        elif self.x == 2  and self.y == 15:
-#            return "Zacetno polje"
-#        else: return "Navadno"
 
+
+########################################### ~ POLJE ~ ########################################### 
 
 def ustvari_polja(maxx, maxy):
     sez = []
     for i in range(maxx):
         for j in range(maxy):
-            sez.append(Polje([i,j]))
+            sez.append([i,j])
     return sez
 
+
+########################################### ~ STADION ~ ###########################################
 class Stadion:
     def __init__(self, maxx, maxy):
         self.polja = ustvari_polja(maxx, maxy)
+
+
+########################################### ~ IGRALEC ~ ###########################################
+class Igralec:
+    def __init__(self, ime, figure):
+        self.ime = ime
+        self.figure = figure
+
+
+########################################### ~ IGRA ~ ###########################################
+class Game:
+    def __init__(self, igralca, stadion):
+        self.igralca = igralca # Igralca je seznam igralcev
+        self.stadion = stadion # Stadion je seznam vseh polj
+
+    def stanje(self):
+        for igralec in self.igralca:
+            for figura in igralec.figure:
+                if [figura.x, figura.y] not in self.stadion: return False
+        return True
+                    
+
+    def premik_figure(self, igralec, figura, x, y):
+        igralec.figure[figura].premakni(x, y)
+
+    def poteza(self, igralec, figuri, premika):
+        # Najprej je met kocke
+        kocke = met_kock()
+        # Po metu kock izbere potezi
+        met1, met2 = kocke[0], kocke[1]
+        self.premik_figure(igralec, figura[0], premika[0][0], premika[0][1])
+        self.premik_figure(igralec, figura[1], premika[1][0], premika[1][1])
 
 
 
